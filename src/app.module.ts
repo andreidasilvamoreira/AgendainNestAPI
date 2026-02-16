e3663d29fc48d34c63ad4ed0c7aa4ed69afce683
andreidasilvamoreira <dedeigalinha@hotmail.com>
2026-02-16 20:34:03 -0300

    Inicialização do projeto com nestJS no back-end

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, ServicesModule, AppointmentsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
