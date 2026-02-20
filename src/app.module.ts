dd6c6710e664cf951627d69ba1789ac9cad59269
andreidasilvamoreira <dedeigalinha@hotmail.com>
2026-02-20 08:59:29 -0300

    chore(prisma): setup PrismaModule and default client configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ServicesModule } from './services/services.module';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ServicesModule, AppointmentsModule],
+  imports: [AuthModule, UsersModule, ServicesModule, AppointmentsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service'; 
 
 @Injectable()
-export class ServicesService {}
+export class ServicesService {
+
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: any) {
+    return this.prisma.servico.create({
+      data
+    });
+  }
+}
