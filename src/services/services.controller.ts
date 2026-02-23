7195abb3c6bac28cbd0f5deff82e50420583f64d
andreidasilvamoreira <dedeigalinha@hotmail.com>
2026-02-23 12:15:46 -0300

    feat(services): add REST controller

--- src/services/services.controller.ts
+++ src/services/services.controller.ts
@@ -1,12 +1,35 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CreateServiceDto } from './dto/create.service.dto';
+import { ServicesService } from './services.service';
+import { UpdateServiceDto } from './dto/update.service.dto';
 
 @Controller('services')
 export class ServicesController {
 
+    constructor(private readonly serviceService: ServicesService){}
+    
+    @Get()
+    findAll(){
+        return this.serviceService.findAll()
+    }
+
+    @Get(':id')
+    find(@Param('id', ParseIntPipe) id: number){
+        return this.serviceService.find(id)
+    }
+
     @Post()
     create(@Body() dto: CreateServiceDto) {
-        console.log(dto);
-        return dto;
+        this.serviceService.create(dto)
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateServiceDto){
+        return this.serviceService.update(id, dto)
+    }
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) id:number) {
+        return this.serviceService.delete(id)
     }
 }
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,14 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service'; 
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service'; 
+import { CreateServiceDto } from './dto/create.service.dto';
+import { UpdateServiceDto } from './dto/update.service.dto';
 
 @Injectable()
 export class ServicesService {
 
   constructor(private prisma: PrismaService) {}
 
-  async create(data: any) {
-    return this.prisma.servico.create({
-      data
-    });
+   async find(id: number) {
+    const item = await this.prisma.servico.findUnique({ where: { id }})
+    if(!item) {throw new NotFoundException('Serviço não encontrado')}
+    return item
+  }
+
+  async findAll() {
+    return this.prisma.servico.findMany({ orderBy: { id: 'desc' }})
+  }
+
+  async create(dto: CreateServiceDto) {
+    return this.prisma.servico.create({ data: dto });
+  }
+
+  async update(id: number, dto: UpdateServiceDto) {
+    await this.find(id)
+    return this.prisma.servico.update({ where: { id }, data: dto});
+  }
+
+  async delete(id: number) {
+    await this.find(id)
+    return this.prisma.servico.delete({ where: { id }})
   }
 }
