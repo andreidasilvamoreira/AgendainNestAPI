cfdf37021153b42f26790523ca182af104c61f5c
Laiza <l.aiza.saraiva@gmail.com>
2026-03-05 10:14:47 -0300

    feat: add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import NavBar from "./components/NavBar/NavBar"
 
 
 import './App.css'
@@ -6,10 +6,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-  
+    <div className="centralizar">
+    <NavBar />
     
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,22 @@
+import Button from "../Button/Button";
+import "./navBar.css";
+
+const NavBar = () => {
+  return (
+    <section className="menu">
+      <div >
+        <h1>SALÃO 10</h1>
+      </div>
+      <nav>
+        <ul className="menu-position">
+          <li>Serviços</li>
+          <li>Agendar</li>
+          <li>Perfil</li>
+          <li><Button>Sair</Button></li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default NavBar;
